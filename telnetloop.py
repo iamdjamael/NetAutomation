9f7bb61f424f7e37c1b5a3e37bee8ab967b912e3
iamdjamael <djdjamil1@gmail.com>
2020-06-13 11:28:04 +0800

    netauto create vlan with telnet and loop

---
+++ telnetloop.py
@@ -0,0 +1,27 @@
+import getpass
+import telnetlib
+
+HOST = "192.168.10.2"
+user = input("Telnet Username : ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"enable\n")
+tn.write(b"cisco\n")
+tn.write(b"conf t\n")
+
+for n in range (2,11):
+    tn.write(b"vlan " + str(n).encode('ascii') + b"\n")
+    tn.write(b"name py_VLAN_" + str(n).encode('ascii') + b"\n")
+
+tn.write(b"end\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
